52c72fe3c14e1bc19c9e6e488611ddbd0a29315a
Live session user <lego@lego.(none)>
2017-12-27 20:14:20 +0200

    some really bad error handling & screen code

---
+++ src/org/mipyykko/roboexplorer/util/RoboException.java
@@ -0,0 +1,31 @@
+package org.mipyykko.roboexplorer.util;
+
+import lejos.nxt.LCD;
+
+import org.mipyykko.roboexplorer.config.ErrorCode;
+import org.mipyykko.roboexplorer.ui.Screen;
+
+
+public class RoboException extends Exception {
+
+	private String message;
+	private ErrorCode code;
+	
+	public RoboException(String message) {
+		super(message);
+	}
+
+	// somehow exception constructor with cause isn't there?!
+	public RoboException(String message, /* Throwable cause, */ErrorCode code) {
+		super(message/*, cause*/);
+		this.message = message;
+		this.code = code;
+		showError();
+	}
+	
+	public void showError() {
+		LCD.clear();
+		LCD.drawString("ERROR!", 0, 0);
+		Screen.showText(message, 0, 2);
+	}
+}
